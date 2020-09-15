f861c9ce5d792f9d1f429ca35fc785dae0cc125e
IgorGarciaCosta <igonildo7@gmail.com>
2020-09-14 22:08:41 -0300

    fist adjustments

---
+++ src/components/MainComponent.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function MainComponent(){
+  return(
+    <div className="checkBoxes">
+      <input type="checkbox"/>
+      <p>Placeholder text</p>
+
+      <input type="checkbox"/>
+      <p>Placeholder text</p>
+
+      <input type="checkbox"/>
+      <p>Placeholder text</p>
+
+      <input type="checkbox"/>
+      <p>Placeholder text</p>
+    </div>
+
+  )
+}
+
+export default MainComponent
